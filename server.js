9a1bcb9abb98dec05bd420f66a1da06b989372d1
gauravshinde1816 <gauravshinde1816@gmail.com>
2021-07-22 21:52:58 +0530

    back done

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/user");
+
+//middleware
+app.use(express.json());
+
+//connect to database
+connectDB();
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+//routes
+app.use("/api/user", userRoutes);
+
+//listen to PORT
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`server started at ${PORT}`);
+});
